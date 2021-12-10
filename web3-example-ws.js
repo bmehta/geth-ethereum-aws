2a0cbcf1c0077c2400e80375788d99bb38a55447
bmehta <bmehta@gannett.com>
2021-12-10 10:06:06 -0500

    Added methods to get block number and chain id.

--- web3-example-ws.js
+++ web3-example-ws.js
@@ -4,5 +4,9 @@ const endpoint = process.env.AMB_WS_ENDPOINT;
 console.log(`ws endpoint: ${endpoint}`);
 const web3 = new Web3(new AWSWebsocketProvider(endpoint));
 web3.eth.getNodeInfo().then(console.log).then(() => {
-	web3.currentProvider.connection.close();
+	web3.eth.getBlockNumber().then(console.log).then(() => {
+		web3.eth.getChainId().then(console.log).then(() => {
+			web3.currentProvider.connection.close();
+		})
+	})
 });
